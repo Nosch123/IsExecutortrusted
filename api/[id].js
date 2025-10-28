5e162d9e9dac62d7839dbe3eff7486df42f7d610
Nosch123 <noah240611@web.de>
2025-10-28 19:15:45 +0100

    Create [id].js

---
+++ api/[id].js
@@ -0,0 +1,11 @@
+export default function handler(req, res) {
+  const { id } = req.query;
+  const data = require('../../data/executors.json');
+  const item = data.find(x => x.id === String(id).toLowerCase());
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Cache-Control', 'public, max-age=60');
+
+  if (!item) return res.status(404).json({ error: 'Executor not found' });
+  return res.status(200).json(item);
+}
