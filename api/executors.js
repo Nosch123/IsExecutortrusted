244683bdf291791c13f099e6458c2f5c79a6a8e3
Nosch123 <noah240611@web.de>
2025-10-28 19:39:24 +0100

    Update executors.js

--- api/executors.js
+++ api/executors.js
@@ -1,8 +1,8 @@
 // api/executors.js
+import data from '../data/executors.json' assert { type: 'json' };
+
 export default function handler(req, res) {
   try {
-    const data = require('../data/executors.json');
-
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Cache-Control', 'public, max-age=60');
     res.status(200).json(data);
