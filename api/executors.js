66b2e7fb2eb5568043a2dbedc38c81b8d381ae25
Nosch123 <noah240611@web.de>
2025-10-28 19:42:22 +0100

    Change JSON import syntax in executors.js

--- api/executors.js
+++ api/executors.js
@@ -1,5 +1,5 @@
 // api/executors.js
-import data from '../data/executors.json' assert { type: 'json' };
+import data from '../data/executors.json' with { type: 'json' };
 
 export default function handler(req, res) {
   try {
