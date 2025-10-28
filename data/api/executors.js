b3ba1b068efb0bc1df2f199921c12a19fb7df3ee
Nosch123 <noah240611@web.de>
2025-10-28 19:14:10 +0100

    Delete data/api directory

--- data/api/executors.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  const data = require('../data/executors.json');
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Cache-Control', 'public, max-age=60');
-  res.status(200).json(data);
-}
