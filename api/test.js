28da06d32f23ae25d422892c7052328c91759faf
Nosch123 <noah240611@web.de>
2025-10-29 11:18:41 +0100

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,5 +1,6 @@
 export default function handler(req, res) {
-  const text = `nosch is so sigma 67 67 67 67`;
+  const text = `nosch is so sigma 67 67 67 67
+  67 lwk boring ngl`;
   
   res.status(200).send(text);
 }
