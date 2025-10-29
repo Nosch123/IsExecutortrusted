80a466d1dd6541aaf23b8d96425c3e2ccd9dc083
Nosch123 <noah240611@web.de>
2025-10-29 13:45:27 +0100

    Update jjsploit.js

--- api/jjsploit.js
+++ api/jjsploit.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
-  const text = `Status: trusted<br>Test Date: 28.10.2025`;
+  const text = `Status: Partially Trusted<br>Test Date: 29.10.2025`;
   
   res.status(200).send(text);
 }
