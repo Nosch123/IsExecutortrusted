3281a210af71f4b4cc59b600831bd5bf19919f8f
Nosch123 <noah240611@web.de>
2025-10-31 17:55:25 +0100

    bunni.js aktualisieren

--- api/bunni.js
+++ api/bunni.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
-  const text = `Status: Partially Trusted<br>Test Date: 28.10.2025`;
+  const text = `Status: Partially Trusted<br>Test Date: 31.10.2025`;
   
   res.status(200).send(text);
 }
